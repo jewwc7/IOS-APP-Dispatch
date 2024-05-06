35325bd667cb1331b883c60f403e1aecf155625e
joshua <joshua.wilson@dispatchit.com>
2024-05-06 17:06:44 -0500

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Joshua Wilson on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

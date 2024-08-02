9f1b28d4b2d9959a4b721502384602fbcdfabfff
joshua <joshua.wilson@dispatchit.com>
2024-08-02 11:44:31 -0500

    driver can claim order and order and driver gets updated

---
+++ MyFirstApp/Components/OrderCard.swift
@@ -0,0 +1,18 @@
+//
+//  OrderCard.swift
+//  MyFirstApp
+//
+//  Created by Joshua Wilson on 8/2/24.
+//
+
+import SwiftUI
+
+struct OrderCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderCard()
+}

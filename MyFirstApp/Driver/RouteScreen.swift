8b207a72fd2af34886d74c56655feb86f4709d09
joshua <joshua.wilson@dispatchit.com>
2024-08-16 17:34:04 -0500

    formatting

--- MyFirstApp/Driver/RouteScreen.swift
+++ MyFirstApp/Driver/RouteScreen.swift
@@ -5,13 +5,13 @@
 //  Created by Joshua Wilson on 8/2/24.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
-//Where I left off:THe order statuses are being updated locally but not globally.(customer screen doesn;t show update logs and the button text) Why?
+// Where I left off:THe order statuses are being updated locally but not globally.(customer screen doesn;t show update logs and the button text) Why?
 
 struct RouteScreen: View {
-    @Environment(\.modelContext) private var context //how to CRUD state
+    @Environment(\.modelContext) private var context // how to CRUD state
     @EnvironmentObject var appState: AppStateModel
     //    @State private var orderNumber: String = "123"
     //    @State private var pickupLocation: String = "1234 main st"
@@ -25,23 +25,20 @@ struct RouteScreen: View {
     //    @State private var dropoffCompanyOrOrg: String = "Diamond boys"
     //    @State private var dropoffNotes: String = "Be careful with my diamonds!"
     //
-    
-    
+
     var body: some View {
         //        @State var tempOrder = Order( orderNumber: orderNumber, pickupLocation: pickupLocation, pickupPhoneNumber: pickupPhoneNumber, pickupContactName: pickupContactName, pickupCompanyOrOrg: pickupCompanyOrOrg, dropoffLocation: dropoffLocation, dropoffPhoneNumber: dropoffPhoneNumber, dropoffContactName: dropoffContactName, dropoffCompanyOrOrg:dropoffCompanyOrOrg, pay: 100, customer: Customer(name: "PlaceHolder customer"))
-        
+
         ScrollView {
             if let loggedInDriver = appState.loggedInDriver {
-                ForEach(loggedInDriver.routes){ route in
-                    Section(header: Text(convertDateToDateOnlyString(day:route.startDate)).bold().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)) {
-                        ForEach(route.orders, id: \.orderId){ order in
-                            DriveStopCard(order:order)
-                            
+                ForEach(loggedInDriver.routes) { route in
+                    Section(header: Text(convertDateToDateOnlyString(day: route.startDate)).bold().font(/*@START_MENU_TOKEN@*/ .title/*@END_MENU_TOKEN@*/)) {
+                        ForEach(route.orders, id: \.orderId) { order in
+                            DriveStopCard(order: order)
                         }
-                        
                     }
                 }
-            }else {
+            } else {
                 Text("Driver not logged in")
             }
             //    DriverOrderCard(order:tempOrder)
@@ -52,4 +49,3 @@ struct RouteScreen: View {
 #Preview {
     RouteScreen().modelContainer(for: [Order.self, Customer.self, Driver.self], inMemory: true).environmentObject(AppStateModel())
 }
-
--- MyFirstApp/Models/Order.swift
+++ MyFirstApp/Models/Order.swift
@@ -5,13 +5,14 @@
 //  Created by Joshua Wilson on 5/20/24.
 //
 
-import SwiftData
-import Foundation
-import CoreData
-import SwiftUI
 import Combine
-//To resolve the issue, you need to ensure that OrderStatus conforms to PersistentModel. In SwiftData (and other similar frameworks like Core Data), enums that are used as properties of persistent models typically need to be marked as conforming to Codable so that they can be serialized and deserialized properly.
-enum OrderStatus: String, Codable  { //
+import CoreData
+import Foundation
+import SwiftData
+import SwiftUI
+
+// To resolve the issue, you need to ensure that OrderStatus conforms to PersistentModel. In SwiftData (and other similar frameworks like Core Data), enums that are used as properties of persistent models typically need to be marked as conforming to Codable so that they can be serialized and deserialized properly.
+enum OrderStatus: String, Codable { //
     case unassinged
     case canceled
     case claimed
@@ -36,19 +37,19 @@ class Order {
     var dropoffCompanyOrOrg: String
     var pay: Int
     var due_at: Date
-    var startedAt: Date? = nil
-    var customer: Customer? = nil
-    var driver: Driver? = nil
-    var status:OrderStatus = OrderStatus.unassinged
+    var startedAt: Date?
+    var customer: Customer?
+    var driver: Driver?
+    var status: OrderStatus = OrderStatus.unassinged
     let inProgressStatuses: Set<OrderStatus> = [OrderStatus.enRouteToPickup, OrderStatus.atPickup, OrderStatus.atDropoff]
-    //Note: SwiftData properties do not support willSet or didSet property observers, unless they have @Transient so had to make a transient prop, update this when status is updated. @Transient does not persist the data, so status is not persisted, so this is a workaround
-   // https://www.hackingwithswift.com/quick-start/swiftdata/how-to-create-derived-attributes-with-swiftdata
+    // Note: SwiftData properties do not support willSet or didSet property observers, unless they have @Transient so had to make a transient prop, update this when status is updated. @Transient does not persist the data, so status is not persisted, so this is a workaround
+    // https://www.hackingwithswift.com/quick-start/swiftdata/how-to-create-derived-attributes-with-swiftdata
     @Transient var transientStatus: OrderStatus = .unassinged {
-             didSet {
-                 //In Swift, didSet is a property observer that executes a block of code immediately after the value of a property changes. Property observers are used to monitor changes in a property’s value, which allows you to respond to changes in state or value without the need to explicitly call a method or function.
-                 self.statusDidChange()
-             }
-         }
+        didSet {
+            // In Swift, didSet is a property observer that executes a block of code immediately after the value of a property changes. Property observers are used to monitor changes in a property’s value, which allows you to respond to changes in state or value without the need to explicitly call a method or function.
+            statusDidChange()
+        }
+    }
      
     init(
         orderNumber: String?,
@@ -62,171 +63,167 @@ class Order {
         dropoffCompanyOrOrg: String,
         pay: Int,
         customer: Customer,
-        status:OrderStatus = OrderStatus.unassinged,
+        status: OrderStatus = OrderStatus.unassinged,
         driver: Driver? = nil,
-        due_at: Date = Calendar.current.date(byAdding: .day, value: 7, to: Date()) ?? Date()) { //7 days from now or right now
-            self.orderId = UUID().uuidString
-            self.orderNumber = orderNumber
-            self.pickupLocation = pickupLocation
-            self.pickupPhoneNumber = pickupPhoneNumber
-            self.pickupContactName = pickupContactName
-            self.pickupCompanyOrOrg = pickupCompanyOrOrg
-            self.dropoffLocation = dropoffLocation
-            self.dropoffPhoneNumber = dropoffPhoneNumber
-            self.dropoffContactName = dropoffContactName
-            self.dropoffCompanyOrOrg = dropoffCompanyOrOrg
-            self.pay = pay
-            self.due_at = due_at//add time to this date
-            self.startedAt = nil
-            self.customer = customer
-            self.driver = driver
-            self.status = status
-        }
+        due_at: Date = Calendar.current.date(byAdding: .day, value: 7, to: Date()) ?? Date())
+    { // 7 days from now or right now
+        self.orderId = UUID().uuidString
+        self.orderNumber = orderNumber
+        self.pickupLocation = pickupLocation
+        self.pickupPhoneNumber = pickupPhoneNumber
+        self.pickupContactName = pickupContactName
+        self.pickupCompanyOrOrg = pickupCompanyOrOrg
+        self.dropoffLocation = dropoffLocation
+        self.dropoffPhoneNumber = dropoffPhoneNumber
+        self.dropoffContactName = dropoffContactName
+        self.dropoffCompanyOrOrg = dropoffCompanyOrOrg
+        self.pay = pay
+        self.due_at = due_at // add time to this date
+        self.startedAt = nil
+        self.customer = customer
+        self.driver = driver
+        self.status = status
+    }
     
     // Method
     func introduce() {
         print("Hello, my name is \(orderId) and I am being picked up from \(pickupLocation)  and am to be dropped off at \(dropoffLocation) for a cost of $\(pay).")
     }
     
-    func late()->Bool{
-        return self.due_at > Date()
+    func late() -> Bool {
+        return due_at > Date()
     }
     
-    func claim(driver:Driver)-> ResultWithMessage{
+    func claim(driver: Driver) -> ResultWithMessage {
         let isClaimable = self.isClaimable()
         if isClaimable.result == .success {
             self.driver = driver
-            self.status = OrderStatus.claimed
+            status = OrderStatus.claimed
             return isClaimable
-        }else {
+        } else {
             return isClaimable
         }
-        
     }
     
-    private func isClaimable()-> ResultWithMessage{
-        if(self.driver != nil){
+    private func isClaimable() -> ResultWithMessage {
+        if driver != nil {
             return ResultWithMessage(result: .failure, message: "Driver already assigned")
         }
-        if self.status == OrderStatus.canceled {
+        if status == OrderStatus.canceled {
             return ResultWithMessage(result: .failure, message: "Order was canceled")
-        }else {
+        } else {
             return ResultWithMessage(result: .success)
         }
     }
     
-    func handleStatusTransition(){
-        if(status == .delivered){
+    func handleStatusTransition() {
+        if status == .delivered {
             return print("Order already delivered")
-        }
-        else if(canceled()){
-           return print("Canceled order cant be delivered")
-        }
-        else if(unassigned()){
-            self.status = .claimed
-        }
-        else if(claimed()){ //unassigned only here for testing purposes
+        } else if canceled() {
+            return print("Canceled order cant be delivered")
+        } else if unassigned() {
+            status = .claimed
+        } else if claimed() { // unassigned only here for testing purposes
             setEnRouteToPickup()
-        }
-        else if(self.status == .enRouteToPickup){
+        } else if status == .enRouteToPickup {
             setAtPickup()
-        }else if(self.status == .atPickup){
+        } else if status == .atPickup {
             setAtDropOff()
-        }
-        else if(self.status == .atDropoff){
+        } else if status == .atDropoff {
             setDelivered()
         }
         
         do {
             try modelContext?.save()
-            self.transientStatus = self.status
+            transientStatus = status
         } catch {
             print(error)
         }
     }
     
-    func claimed()-> Bool{
-        //commented out for testing
-        return status == .claimed //&& driver != nil
-       // return true
+    func claimed() -> Bool {
+        // commented out for testing
+        return status == .claimed // && driver != nil
+        // return true
     }
-    func unassigned()-> Bool{
+
+    func unassigned() -> Bool {
         return status == .unassinged
     }
     
-    func assigned()-> Bool{
+    func assigned() -> Bool {
         return status == .claimed && driver != nil
     }
     
-    func started()->Bool {
+    func started() -> Bool {
         return startedAt != nil
     }
-    func canceled()->Bool {
+
+    func canceled() -> Bool {
         return status == .canceled
     }
-    func delivered()->Bool {
+
+    func delivered() -> Bool {
         return status == .delivered
     }
-    func inProgess()-> Bool{
+
+    func inProgess() -> Bool {
         print(inProgressStatuses.contains(status), status)
         return inProgressStatuses.contains(status)
     }
     
-    func dueAtFormatted()->String {
-      let formatter3 = DateFormatter()
-      formatter3.dateFormat = "HH:mm E, d MMM y"
-        return formatter3.string(from: self.due_at)
+    func dueAtFormatted() -> String {
+        let formatter3 = DateFormatter()
+        formatter3.dateFormat = "HH:mm E, d MMM y"
+        return formatter3.string(from: due_at)
     }
     
-    private func setEnRouteToPickup(){
- 
-            if   self.driver == nil {
-                print("order has no driver")
-            }
-            else {
-                print("setting @ setEnRouteToPickup")
-                self.status = .enRouteToPickup
-                self.startedAt = Date()
-            }
-        
+    private func setEnRouteToPickup() {
+        if driver == nil {
+            print("order has no driver")
+        } else {
+            print("setting @ setEnRouteToPickup")
+            status = .enRouteToPickup
+            startedAt = Date()
+        }
     }
-    private func setAtPickup(){
+
+    private func setAtPickup() {
         print("setting @ pickup")
-        if self.status != .enRouteToPickup  {
+        if status != .enRouteToPickup {
             print("please set enroute to pickup first")
-        }else {
-            self.status = .atPickup
+        } else {
+            status = .atPickup
         }
     }
-    private func setAtDropOff(){
+
+    private func setAtDropOff() {
         print("setting @ dropoff")
-        if self.status != .atPickup  {
+        if status != .atPickup {
             print("please complete pickup first")
-        }else {
-            self.status = .atDropoff
+        } else {
+            status = .atDropoff
         }
-        
-        
     }
-    private func setDelivered(){
+
+    private func setDelivered() {
         print("setting @ delivered")
-        if self.status != .atDropoff  {
+        if status != .atDropoff {
             print("please setAtDropoff first")
-        }else {
-            self.status = .delivered
+        } else {
+            status = .delivered
         }
-        
-        
     }
-     func statusDidChange() {
+
+    func statusDidChange() {
         UIApplication.shared.inAppNotification(adaptForDynmaicIsland: false, timeout: 5, swipeToClose: true) {
             Text("Hi \(self.customer?.name ?? "") your order is now \(humanizeCamelCase(self.status.rawValue))").padding().background(.white)
         }
         // Alternatively, you could call a delegate method, execute a closure, etc.
     }
     
-    func driverRouteStatus ()-> String{
-        let title:String = {
+    func driverRouteStatus() -> String {
+        let title: String = {
             switch self.status {
             case .enRouteToPickup:
                 "En route to pickup"
@@ -240,7 +237,4 @@ class Order {
         }()
         return title
     }
-
 }
-
-
